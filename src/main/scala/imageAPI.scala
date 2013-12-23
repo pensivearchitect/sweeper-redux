78d9e0e8d54b96e1b2fb26a37ff4268a26b0b5c7
Joshua Bell <pensivearchitect@gmail.com>
2013-12-23 14:45:16 -0800

    Inital working version of Image API

---
+++ src/main/scala/imageAPI.scala
@@ -0,0 +1,35 @@
+package imageInstantiationAndWritingAPI
+
+import scalafx.application.Platform
+import scalafx.geometry.Insets
+import scalafx.scene.image.Image
+import scalafx.scene.image.ImageView
+import scalafx.scene.layout.Priority
+import scalafx.scene.layout.VBox
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import scalafx.Includes._
+import generateImage._
+
+class createWindow extends VBox with createImage {
+  def imageDisplay = new VBox {
+    val verticalPriority = Priority.ALWAYS
+    val horizontalPriority = Priority.ALWAYS
+    spacing = 10
+    padding = Insets(20)
+    content = List(
+      new ImageView {
+        val getImage = {
+
+          val currentImage: BufferedImage = getUserSelectedAreaAsImage
+          def obtainedImage = returnCurrentImage(currentImage)
+
+        }
+        fitHeight = 300
+        fitWidth = 300
+        preserveRatio = true
+        smooth = true
+      })
+  }
+}
+
