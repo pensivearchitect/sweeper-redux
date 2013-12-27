f38b7e44eeb832a1cffb5424ce895207aa1ea445
Joshua Bell <pensivearchitect@gmail.com>
2013-12-26 23:18:33 -0800

    ImageWriter actor behaves correctly now

--- src/main/scala/actorAPI.scala
+++ src/main/scala/actorAPI.scala
@@ -1,24 +1,34 @@
 package actorAPI
 
-import dispatch._
 import java.awt.image.BufferedImage
 import java.io._
 import java.util.Calendar
 import javax.imageio.ImageIO
+import akka.actor.Actor
+import akka.actor.Props
+import akka.dispatch._
+import akka.event.Logging
 
 abstract class genericActor extends Actor
   with RequiresMessageQueue[BoundedMessageQueueSemantics] {
-}
-
-trait ActorTraits extends genericActor {
+  import context._
   val log = Logging(context.system, this)
   def actorProperties(name: String): Props = Props(classOf[genericActor], name)
+
 }
 
-abstract class AsynchronousImageWriter extends ActorTraits {
+trait ImageTraits {
   val calendar = Calendar.getInstance()
   val filename = (calendar.getTimeInMillis + ".png").toString
-  ImageIO.write(_: BufferedImage, "png", new File(filename))
- // val actorDispatcher = // TODO: Write dispatcher
+  case class WriteableImage(image: BufferedImage)
+
+}
+
+object AsyncImageWriter extends genericActor with ImageTraits {
+  def receive: Receive = {
+    case WriteableImage =>
+      sender ! ImageIO.write(_: BufferedImage, "png", new File(filename))
+
+  }
 }
 
